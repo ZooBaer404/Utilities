924bf51ba032b3c68412e089d2f3b07cfac188ed
ZooBaer404 <zubairstudytech@gmail.com>
2025-05-26 15:46:01 +0430

    make: add more features and src for a more structured project

---
+++ Templates/make/include/builtin/logger.c
@@ -0,0 +1,25 @@
+#include "logger.h"
+#include "colors.h"
+
+
+void error(FILE *stream, char *type, char *message) {
+    if (type == NULL || message == NULL) {
+	fprintf(stderr, RED "%s: %s", type, message);
+	fprintf(RESET"\n");
+    }
+    
+    fprintf(stream, RED "%s: %s", type, message);
+    fprintf(stream, RESET"\n");
+}
+
+void info(FILE *stream, char *type, char *message) {
+    if (type == NULL || message == NULL) {
+	fprintf(stderr, RED "%s: %s", "info_out", "type or message NULL");
+	fprintf(RESET"\n");
+    }
+    
+    fprintf(stream, GREEN "%s: %s", type, message);
+    fprintf(stream, RESET"\n");
+}
+
+
